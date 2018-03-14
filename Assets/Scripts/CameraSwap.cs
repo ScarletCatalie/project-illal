52f847d04fab0a4b4b1e7c35426076b8369a5d9d
Natalie (Satellite) <claytonnatalie94@outlook.com>
2018-03-14 11:53:23 +0000

    Camera Update
    First / Third Person Camera transition implemented.
    Terrain

---
+++ Assets/Scripts/CameraSwap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour {
+
+    public DragMouseOrbit perspshift;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+           
+            perspshift.enabled = !perspshift.enabled;
+            GameObject.Find("CAMERA").transform.localPosition = new Vector3(-2.85f, 1.3f, 1.1f);
+            GameObject.Find("CAMERA").transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90f));
+
+        }
+
+    }
+}
